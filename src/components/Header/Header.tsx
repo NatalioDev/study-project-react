ec56f7884b31229532949b31dbb20cc26dadfbbc
NatalioDev <nataliodg.dev@gmail.com>
2025-01-06 23:53:49 -0300

    add header and toggle theme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,28 @@
+import { useTheme } from "../../utilites/Context/UseTheme"
+
 
 const Header = () => {
+
+  // Traemos el theme y la function para cambiar el contex
+  const {theme , toggleTheme} =useTheme();
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className="flex items-center justify-between mt-[50px] mr-0 mb-[25px] ml-0 w-[90%] md:mt-16 md:mr-0 md:mb-10 md:ml-0 md:w-[550px]">
+      <h1 className="text-slate-900 text-4xl tracking-[12px] uppercase dark:text-yellow-400">
+        ToDo List
+      </h1>
+      <button className="bg-none border-none cursor-pointer mr-[-7px] outline-none pt-2 px-2 pb-1 transition duration-1000 rounded-md" onClick={toggleTheme}>
+        {/* Mostramos el icono correxpondiente (dark) */}
+        {theme.name === "dark" ? (
+          <img src="icon/icon-moon.svg" alt="Moon Icon" />
+        ):(
+          <img 
+            src="icon/icon-sun.svg" 
+            alt="Sun Icon" 
+            className="text-yellow-400" // Cambiar color a amarillo en modo claro
+          />
+        )}
+      </button>
+    </header>
   )
 }
 
