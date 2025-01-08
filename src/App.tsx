0a756477c11f0d01afcfbd6273a54a020766b2c6
NatalioDev <nataliodg.dev@gmail.com>
2025-01-07 23:20:28 -0300

    update 7/1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import Header from "./components/Header/Header";
 import { ThemeContextProvider } from "./utilites/ContextTheme/ThemeContextProvider";
+import { TodoProvider } from "./utilites/ContextTodo/TodoProvider";
 
 const App = () => {
+
   return (
     <ThemeContextProvider>
       {/* Aplicar colores personalizados */}
       <div className="min-h-screen transition-colors duration-300 bg-customLight text-lightText dark:bg-customDark dark:text-darkText">
         <Header />
         <main className="p-4">
-          <h2 className="text-xl font-bold dark:text-yellow-400">Mi aplicación con tema dinámico</h2>
-          <p className="dark:text-gray-200">
-            ¡Cambia entre el modo claro y oscuro!
-          </p>
+          <TodoProvider>
+            <h2 className="text-xl font-bold dark:text-yellow-400">Mi aplicación con tema dinámico</h2>
+            <p className="dark:text-gray-200">
+              ¡Cambia entre el modo claro y oscuro!
+            </p>
+          </TodoProvider>
+          
         </main>
       </div>
     </ThemeContextProvider>
