6328b6aa24e090593dcfbf621bcbc83dfd18232c
NatalioDev <nataliodg.dev@gmail.com>
2025-01-07 22:48:58 -0300

    add todoInput component

--- src/components/Todo/TodoInput/TodoInput.tsx
+++ src/components/Todo/TodoInput/TodoInput.tsx
@@ -1,8 +1,52 @@
+import { ChangeEvent, FormEvent, useContext, useState } from "react"
+import todoContext from "../../../utilites/ContextTodo/TodoContext"
+import { ToDoContextProps, TodoType } from "../../../utilites/Types/types"
+
+import { v4 as uuidv4 } from "uuid";
 
 const TodoInput = () => {
+
+  const [text, setText] = useState<string>("")
+
+
+  const {addTodo} = useContext(todoContext) as ToDoContextProps
+
+  const handleChange = ( e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = ( e: FormEvent) => {
+    e.preventDefault();
+
+    if(text.trim() !== ""){
+      const newTodo: TodoType ={
+        id: uuidv4(),
+        completed: false,
+        text,
+      };
+
+      addTodo(newTodo);
+
+      setText("")
+    }
+  }
+
   return (
     <>
-      <h1>Todo Input</h1>
+      <div className="">
+        <form onClick={handleSubmit}>
+          <div className="">
+            <button className="">Add</button>
+            <input 
+              type="text" 
+              className="" 
+              placeholder="Create a new ToDo"
+              value={text}
+              onChange={handleChange}
+            />
+          </div>
+        </form>
+      </div>
     </>
   )
 }
