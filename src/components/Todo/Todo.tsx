9fde97dd92babfb49974aae20a83657730eef97d
NatalioDev <nataliodg.dev@gmail.com>
2025-01-07 23:19:35 -0300

    add todo component

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,9 +1,48 @@
+import { useContext } from "react"
+import { ToDoContextProps, TodoProps } from "../../utilites/Types/types"
+import todoContext from "../../utilites/ContextTodo/TodoContext"
+import { Draggable, DraggableProvided } from "react-beautiful-dnd";
 
-const Todo = () => {
+const Todo = ({todoText, completed, index, id}: TodoProps) => {
+
+  const {setCompleted, clearTodo} = useContext(todoContext) as ToDoContextProps;
+
+  const handleClick = () => {
+    setCompleted(id);
+    console.log(id);
+  }
+
+  const handleDeleted = () =>{
+    clearTodo(id);
+  }
+
   return (
-    <div>
-      <h1>Todo</h1>
-    </div>
+    <>
+      <Draggable
+        key={id}
+        draggableId={id}
+        index={index}
+      >
+        {(provided: DraggableProvided) => (
+          <div 
+            className=""
+            ref={provided.innerRef}
+            {...provided.dragHandleProps}
+            {...provided.draggableProps}
+            data-index={index}
+          >
+            <div 
+              className={`circle${completed ? " circle-gradient" : ""}`}
+              onClick={handleClick}
+            ></div>
+
+            <p className="">{todoText}</p>
+
+            <img src="icon/icon-cross.svg" alt="cross" onClick={handleDeleted}/>
+          </div>
+        )}
+      </Draggable>
+    </>
   )
 }
 
