03d0dc1f7c52a6b36a65c12fe38967ba9ccfd9ef
NatalioDev <nataliodg.dev@gmail.com>
2025-01-07 21:30:44 -0300

    add comment

--- src/utilites/ContextTodo/useTodoContext.tsx
+++ src/utilites/ContextTodo/useTodoContext.tsx
@@ -3,6 +3,8 @@ import todoContext from "./TodoContext";
 
 export const useTodoContext = () =>{
     const context = useContext(todoContext);
+
+    // If there is no context (indicating misuse), it throws an error with the message
     if(!context){
       throw new Error('useTodoContext must be used within a TodoProvider');
     }
